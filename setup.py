75e6c739c6a73dfb291080c9d64cd15c4c5c4747
DoubleF3lix <48461905+Double-Felix@users.noreply.github.com>
2021-05-16 15:34:55 -0500

    Fixed invalid data in setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="Kyan",
-    version="1.0.0",
+    version="1.0.1",
     description="A soundboard program written in python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DoubleF3lix/Kyan/",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3.0",
+        "Operating System :: Windows 10",
     ]
 )
