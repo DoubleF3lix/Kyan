518a11a14b86c31c584a58b0321c3b23efb7ca8e
DoubleF3lix <48461905+Double-Felix@users.noreply.github.com>
2021-05-16 16:16:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="Kyan",
-    version="1.0.0",
+    version="1.1.0",
     description="A soundboard program written in python",
     long_description=long_description,
     long_description_content_type="text/markdown",
